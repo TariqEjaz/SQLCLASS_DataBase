4603a391168f1ad48166f1649c3fc964fbec43eb
Tariq Ejaz <mtariq_ejaz@yahoo,com>
2022-04-13 17:37:58 -0400

    q22

--- Quries/Adam.sql
+++ Quries/Adam.sql
@@ -84,41 +84,94 @@ SELECT SALARY
 FROM EMPLOYEES
 WHERE EMPLOYEE_ID =163;
 
+-- 14.write a SQL query to find those employees whose designation is the same as the employee whose ID is 169.
+-- Return first name, last name, department ID and job ID.
 
+SELECT FIRST_NAME,LAST_NAME,DEPARTMENT_ID,JOB_ID
+FROM EMPLOYEES
+WHERE JOB_ID = (
+SELECT JOB_ID
+FROM EMPLOYEES
+WHERE EMPLOYEE_ID=169);
 
-
--- 14.write a SQL query to find those employees whose designation is the same as the employee whose ID is 169. Return first name, last name, department ID and job ID.
-
-
--- 15.write a SQL query to find those employees whose salary matches the smallest salary of any of the departments. Return first name, last name and department ID.
-
+-- 15.write a SQL query to find those employees whose salary matches the smallest salary of any of the departments.
+-- Return first name, last name and department ID.
+SELECT FIRST_NAME,LAST_NAME,DEPARTMENT_ID
+FROM EMPLOYEES
+WHERE SALARY IN (SELECT MIN(SALARY)
+FROM EMPLOYEES
+GROUP BY DEPARTMENT_ID);
 
 -- 16. write a SQL query to find those employees who earn more than the average salary. Return employee ID, first name, last name.
 
-
--- 17. write a SQL query to find those employees who report that manager whose first name is ‘Payam’. Return first name, last name, employee ID and salary.
-
-
-
--- 18. write a SQL query to find all those employees who work in the Finance department. Return department ID, name (first), job ID and department name.
-
+SELECT FIRST_NAME,LAST_NAME,EMPLOYEE_ID
+FROM EMPLOYEES
+    WHERE SALARY > (
+SELECT ROUND ( AVG(SALARY), 2)
+FROM EMPLOYEES);
 
 
--- 19. write a SQL query to find those employees whose ID matches any of the number 134, 159 and 183. Return all the fields.
+-- 17. write a SQL query to find those employees who report that manager whose first name is ‘Payam’.
+-- Return first name, last name, employee ID and salary.
+
+select FIRST_NAME, LAST_NAME, EMPLOYEE_ID, SALARY
+FROM EMPLOYEES
+where MANAGER_ID=(select MANAGER_ID
+                  from EMPLOYEES
+                  where FIRST_NAME='Payam');
+
+-- 18. write a SQL query to find all those employees who work in the Finance department.
+-- Return department ID, name (first), job ID and department name.
 
 
 
--- 20. write a SQL query to find those employees whose salary is in the range of smallest salary, and 2500. Return all the fields.
+-- 19. write a SQL query to find those employees whose ID matches any of the number 134, 159 and 183.
+-- Return all the fields.
+
+SELECT *
+FROM EMPLOYEES
+WHERE EMPLOYEE_ID IN (134,159,183);
 
 
+-- 20. write a SQL query to find those employees whose salary is in the range of smallest salary,
+-- and 2500. Return all the fields.
 
--- 21. write a SQL query to find those employees who do not work in those departments where manager ids are in the range 100, 200 (Begin and end values are included.) Return all the fields of the employees.
+SELECT *
+FROM EMPLOYEES
+    WHERE SALARY BETWEEN   ( SELECT MIN(SALARY)
+FROM EMPLOYEES) AND 2500 ;
 
 
+-- 21. write a SQL query to find those employees
+-- who do not work in those departments where manager ids are in the range 100, 200
+-- (Begin and end values are included.) Return all the fields of the employees.
 
+SELECT *
+FROM EMPLOYEES
+    WHERE DEPARTMENT_ID NOT IN (
+SELECT DEPARTMENT_ID
+FROM DEPARTMENTS
+WHERE EMPLOYEES.MANAGER_ID BETWEEN 100 AND 200);
 
--- 22. write a SQL query to find those employees who get second-highest salary. Return all the fields of the employees.
+-- 22. write a SQL query to find those employees who get second-highest salary.
+-- Return all the fields of the employees.
 
-
+SELECT MAX((SALARY))
+FROM EMPLOYEES
+    WHERE SALARY!= (
+SELECT MAX(SALARY)
+FROM EMPLOYEES);
 
 -- 23. write a SQL query to find those employees who get fifth-highest salary. Return all the fields of the employees.
+
+SELECT  MIN(SALARY)
+FROM (SELECT  DISTINCT SALARY
+      FROM EMPLOYEES
+      ORDER BY SALARY DESC
+     )
+WHERE ROWNUM <=5;
+
+
+
+
+
